4374bec85faa69f7c15fd1e99214cb686b699d99
Vaconcovat <vaconcovat@gmail.com>
2016-09-18 23:38:07 +1000

    Task 4 done
    Implemented colour-based picking
    Task 4 complete
    Cleaned up the html a bit

--- objLoader.js
+++ objLoader.js
@@ -1,6 +1,3 @@
-/**
- * Created by mvandeberg on 1/08/2015.
- */
  var count2 = 0;
  var clicked = 0;
  var count = 0;
@@ -14,23 +11,34 @@
  var color = new Uint8Array(4);
  var mult = 1;
  var mult2 = 1;
- var task = 1;
- var selectedMarker = 0;
+ var task = 0;
  var colorsArray = [];
+ var framebuffer;
+ 
+ //managing markers
+ var selectedMarker = -1; //0=obj1, 1=obj2
+ var ypos1 = 0.5;
+ var ypos2 = -0.5;
 
 function render(gl,scene,timestamp,previousTimestamp) {
     if(count == 0){
            // console.log(scene.object2.modelMatrix);
-        mat4.scale(scene.object.modelMatrix, scene.object.modelMatrix, [0.4, 0.4, 0.4]);
+        mat4.scale(scene.object.modelMatrix, scene.object.modelMatrix, [0.2, 0.2, 0.2]);
         count++;
-		mat4.scale(scene.object2.modelMatrix,scene.object2.modelMatrix, [0.4,0.4,0.4]);
-		mat4.scale(scene.marker1.modelMatrix,scene.marker1.modelMatrix, [0.2,0.2,0.2]);
-		mat4.scale(scene.marker2.modelMatrix,scene.marker2.modelMatrix, [0.2,0.2,0.2]);
-		mat4.scale(scene.marker3.modelMatrix,scene.marker3.modelMatrix, [0.2,0.2,0.2]);
-		mat4.scale(scene.marker4.modelMatrix,scene.marker4.modelMatrix, [0.2,0.2,0.2]);
+		mat4.scale(scene.object2.modelMatrix,scene.object2.modelMatrix, [0.2,0.2,0.2]);
+		mat4.scale(scene.marker1.modelMatrix,scene.marker1.modelMatrix, [0.08,0.08,0.08]);
+		mat4.scale(scene.marker2.modelMatrix,scene.marker2.modelMatrix, [0.08,0.08,0.08]);
+		mat4.scale(scene.marker3.modelMatrix,scene.marker3.modelMatrix, [0.08,0.08,0.08]);
+		mat4.scale(scene.marker4.modelMatrix,scene.marker4.modelMatrix, [0.08,0.08,0.08]);
     }
    
-
+	//move all objects to ypos
+	scene.object.modelMatrix[13] = ypos1;
+	scene.object2.modelMatrix[13] = ypos2;
+	scene.marker1.modelMatrix[13] = ypos1;
+	scene.marker2.modelMatrix[13] = ypos1;
+	scene.marker3.modelMatrix[13] = ypos2;
+	scene.marker4.modelMatrix[13] = ypos2;
 
     mat4.rotate(scene.object2.modelMatrix, scene.object2.modelMatrix,Math.PI/100,[0, 1, 0]);
     
@@ -46,14 +54,8 @@ function render(gl,scene,timestamp,previousTimestamp) {
     gl.uniformMatrix4fv(scene.program.modelMatrixUniform, gl.FALSE,scene.object.modelMatrix);
     
     var normalMatrix = mat3.create();
-    mat3.normalFromMat4(
-        normalMatrix,
-        mat4.multiply(
-            mat4.create(),
-            scene.object.modelMatrix,
-            scene.viewMatrix));
-    gl.uniformMatrix3fv(
-        scene.program.normalMatrixUniform, gl.FALSE, normalMatrix);
+    mat3.normalFromMat4(normalMatrix,mat4.multiply(mat4.create(),scene.object.modelMatrix,scene.viewMatrix));
+    gl.uniformMatrix3fv(scene.program.normalMatrixUniform, gl.FALSE, normalMatrix);
     redVal = 0.3*scene.object.modelMatrix[12];
     blueVal= scene.object.modelMatrix[12];
     mat3.normalFromMat4(normalMatrix,mat4.multiply(mat4.create(),scene.object.modelMatrix,scene.viewMatrix));
@@ -71,11 +73,13 @@ function render(gl,scene,timestamp,previousTimestamp) {
 
     
     gl.bindBuffer(gl.ARRAY_BUFFER, scene.object.vertexBuffer);
-    gl.drawArrays(gl.TRIANGLES, 0, scene.object.vertexCount);
+    if(task > 0){
+		gl.drawArrays(gl.TRIANGLES, 0, scene.object.vertexCount);
+	}
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
     
 		//OBJECT2
-    scene.object2.modelMatrix[12] = scene.object2.modelMatrix[12]+(1.5*movingDirec2*mult2);
+    scene.object2.modelMatrix[12] = scene.object2.modelMatrix[12]+(1.2*movingDirec2*mult2);
     //scene.object2.modelMatrix[13] = -0.5;
 
 
@@ -161,6 +165,8 @@ function render(gl,scene,timestamp,previousTimestamp) {
 	scene.program.colorVal = gl.getUniformLocation(scene.program, 'colorVal');
     gl.uniform3fv(scene.program.colorVal, colorVal);
 	gl.bindBuffer(gl.ARRAY_BUFFER, scene.marker4.vertexBuffer);
+	//reset i
+	gl.uniform1i(gl.getUniformLocation(scene.program, "i"),0);
     if(task > 3){
 		gl.drawArrays(gl.TRIANGLES, 0, scene.marker4.vertexCount);
 	}
@@ -201,20 +207,31 @@ function init(object, object2, marker1, marker2, marker3, marker4) {
 
     vertexColors = vec3.fromValues(1.0, 0.0, 0.0);
     var surface = document.getElementById('rendering-surface');
-    var gl = surface.getContext('experimental-webgl');
+    var gl = surface.getContext('experimental-webgl', {preserveDrawingBuffer: true});
+	//gl = WebGLUtils.setupWebGL( surface );
 
     gl.viewport(0,0,surface.width,surface.height);
-    gl.enable(gl.DEPTH_TEST);
+	gl.enable(gl.DEPTH_TEST);
     gl.enable(gl.CULL_FACE);
-    gl.cullFace(gl.BACK);
+	gl.cullFace(gl.BACK);
     gl.clearColor(0.0, 0.0, 0.0, 0.0);
 
-    var program = createProgram(
-        gl,
-        [{container: 'vertex-shader', type: gl.VERTEX_SHADER},
-            {container: 'fragment-shader', type: gl.FRAGMENT_SHADER}]);
+    var program = createProgram(gl,[{container: 'vertex-shader', type: gl.VERTEX_SHADER},{container: 'fragment-shader', type: gl.FRAGMENT_SHADER}]);
 
     gl.useProgram(program);
+	
+	//texture for picking
+	var texture = gl.createTexture();
+    gl.bindTexture( gl.TEXTURE_2D, texture );
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 512, 512, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+    gl.generateMipmap(gl.TEXTURE_2D);
+	
+	// Allocate a frame buffer object
+    framebuffer = gl.createFramebuffer();
+    gl.bindFramebuffer( gl.FRAMEBUFFER, framebuffer);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
     
     program.positionAttribute = gl.getAttribLocation(program, 'pos');
     gl.enableVertexAttribArray(program.positionAttribute);
@@ -235,16 +252,16 @@ function init(object, object2, marker1, marker2, marker3, marker4) {
     gl.bufferData(gl.ARRAY_BUFFER, object2.vertices, gl.STATIC_DRAW);
 	
 	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBufferMarker1);
-    gl.bufferData(gl.ARRAY_BUFFER, object2.vertices, gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, marker1.vertices, gl.STATIC_DRAW);
 	
 	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBufferMarker2);
-    gl.bufferData(gl.ARRAY_BUFFER, object2.vertices, gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, marker2.vertices, gl.STATIC_DRAW);
 	
 	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBufferMarker3);
-    gl.bufferData(gl.ARRAY_BUFFER, object2.vertices, gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, marker3.vertices, gl.STATIC_DRAW);
 	
 	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBufferMarker4);
-    gl.bufferData(gl.ARRAY_BUFFER, object2.vertices, gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, marker4.vertices, gl.STATIC_DRAW);
 
     gl.vertexAttribPointer(
         program.positionAttribute, 3, gl.FLOAT, gl.FALSE,
@@ -256,9 +273,7 @@ function init(object, object2, marker1, marker2, marker3, marker4) {
 
 
     var projectionMatrix = mat4.create();
-    mat4.perspective(
-        projectionMatrix, 0.75, surface.width/surface.height,
-        0.1, 100);
+    mat4.perspective(projectionMatrix, 0.75, surface.width/surface.height,0.1, 100);
     program.projectionMatrixUniform = gl.getUniformLocation(
         program, 'projectionMatrix');
     gl.uniformMatrix4fv(
@@ -266,43 +281,40 @@ function init(object, object2, marker1, marker2, marker3, marker4) {
         projectionMatrix);
 
     var viewMatrix = mat4.create();
-    program.viewMatrixUniform = gl.getUniformLocation(
-        program, 'viewMatrix');
-    gl.uniformMatrix4fv(
-        program.viewMatrixUniform, gl.FALSE, viewMatrix);
+    program.viewMatrixUniform = gl.getUniformLocation(program, 'viewMatrix');
+    gl.uniformMatrix4fv(program.viewMatrixUniform, gl.FALSE, viewMatrix);
 
     var modelMatrix = mat4.create();
     mat4.identity(modelMatrix);
-    mat4.translate(modelMatrix, modelMatrix, [0, 0.5, -4]);
+    mat4.translate(modelMatrix, modelMatrix, [0, ypos1, -4]);
     program.modelMatrixUniform = gl.getUniformLocation(
         program, 'modelMatrix');
     
-    gl.uniformMatrix4fv(
-        program.modelMatrixUniform, gl.FALSE, modelMatrix);
+    gl.uniformMatrix4fv(program.modelMatrixUniform, gl.FALSE, modelMatrix);
 
     var modelMatrix2 = mat4.create();
     mat4.identity(modelMatrix2);
-    mat4.translate(modelMatrix2, modelMatrix2, [0, -0.5, -4]);
+    mat4.translate(modelMatrix2, modelMatrix2, [0, ypos2, -4]);
 	
 	//marker1
 	var modelMatrixMarker1 = mat4.create();
     mat4.identity(modelMatrixMarker1);
-    mat4.translate(modelMatrixMarker1, modelMatrixMarker1, [-2, 0.5, -4]);
+    mat4.translate(modelMatrixMarker1, modelMatrixMarker1, [-1.2, ypos1, -4]);
 	
 	//marker2
 	var modelMatrixMarker2 = mat4.create();
     mat4.identity(modelMatrixMarker2);
-    mat4.translate(modelMatrixMarker2, modelMatrixMarker2, [2, 0.5, -4]);
+    mat4.translate(modelMatrixMarker2, modelMatrixMarker2, [1.2, ypos1, -4]);
 	
 	//marker3
 	var modelMatrixMarker3 = mat4.create();
     mat4.identity(modelMatrixMarker3);
-    mat4.translate(modelMatrixMarker3, modelMatrixMarker3, [-2, -0.5, -4]);
+    mat4.translate(modelMatrixMarker3, modelMatrixMarker3, [-1.2, ypos2, -4]);
 	
 	//marker4
 	var modelMatrixMarker4 = mat4.create();
     mat4.identity(modelMatrixMarker4);
-    mat4.translate(modelMatrixMarker4, modelMatrixMarker4, [2, -0.5, -4]);
+    mat4.translate(modelMatrixMarker4, modelMatrixMarker4, [1.2, ypos2, -4]);
 	
 
 
@@ -429,202 +441,155 @@ function init(object, object2, marker1, marker2, marker3, marker4) {
     };
 
 
-
-
-    //surface.addEventListener("mousedown", function(){
-
     surface.addEventListener("mousedown", function(event){
-        gl.clear(gl.COLOR_BUFFER_BIT);
-		gl.bindBuffer(gl.ARRAY_BUFFER, null);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+        gl.clear( gl.COLOR_BUFFER_BIT);
+				
+        var x = event.clientX;
+        var y = surface.height - event.clientY; 
 		
 		//Render object 1
 		gl.uniformMatrix4fv(scene.program.modelMatrixUniform, gl.FALSE,scene.object.modelMatrix);
-    
-        colorVal = vec3.create();
-        colorVal[0] = 10;
-        colorVal[1] = 10;
-        colorVal[2] = 10;
 
-        program.colorVal = gl.getUniformLocation(program, 'colorVal');
-        gl.uniform3fv(program.colorVal, colorVal);
+		gl.uniform1i(gl.getUniformLocation(scene.program, "i"),1);
 
-        gl.bindBuffer(gl.ARRAY_BUFFER, scene.object.vertexBuffer);
         gl.drawArrays(gl.TRIANGLES, 0, scene.object.vertexCount);
-        gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-        var x = event.clientX;
-        var y = surface.height -event.clientY; 
-        gl.readPixels(x, y, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, color);
-		console.log(color[0]);
-        if(task >2){
-            if(color[0] == 255){            
-                    if(event.which == 1){
-                        if(mult < 1.6){
-                            mult = mult+0.5;
-                        }
-                    }else if(event.which == 3){
-                        if (mult > 0.1){
-                            mult = mult - 0.5;
-                        }
-                    }   
-                }
-        }
         //-------------------------
 
 		//Render object 2
         gl.uniformMatrix4fv(scene.program.modelMatrixUniform, gl.FALSE,scene.object2.modelMatrix);
         
-        colorVal = vec3.create();
-        colorVal[0] = 20;
-        colorVal[1] = 20;
-        colorVal[2] = 20;
-
-        program.colorVal = gl.getUniformLocation(program, 'colorVal');
-        gl.uniform3fv(program.colorVal, colorVal);
+		gl.uniform1i(gl.getUniformLocation(scene.program, "i"),2);
 		
-        gl.bindBuffer(gl.ARRAY_BUFFER, scene.object2.vertexBuffer);
         gl.drawArrays(gl.TRIANGLES, 0, scene.object2.vertexCount);
-        gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-        gl.readPixels(x, y, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, color);
-		console.log(color[0]);
-        if(task > 2){
-            if(color[0] == 20){
-                if(event.which == 1){
-                    if(mult2 < 1.6){
-                        mult2 = mult2 + 0.1;
-                    }
-                }else if(event.which == 3){
-                    if (mult2 > 0.1){
-                        mult2 = mult2 - 0.1;
-                    }
-                }   
-             }
-        }
-		//---------------------------
+        //---------------------------
         
 		//Render marker 1
         gl.uniformMatrix4fv(scene.program.modelMatrixUniform, gl.FALSE,scene.marker1.modelMatrix);
-        
-        colorVal = vec3.create();
-        colorVal[0] = 30;
-        colorVal[1] = 30;
-        colorVal[2] = 30;
 
-        program.colorVal = gl.getUniformLocation(program, 'colorVal');
-        gl.uniform3fv(program.colorVal, colorVal);
+		gl.uniform1i(gl.getUniformLocation(scene.program, "i"),3);
 		
-        gl.bindBuffer(gl.ARRAY_BUFFER, scene.marker1.vertexBuffer);
         gl.drawArrays(gl.TRIANGLES, 0, scene.marker1.vertexCount);
-        gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-        gl.readPixels(x, y, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, color);
-		console.log(color[0]);
-        if(task > 3){
-            if(color[0] == 30){
-                //   
-             }
-        }
 		//---------------------------
 		
 		//Render marker 2
         gl.uniformMatrix4fv(scene.program.modelMatrixUniform, gl.FALSE,scene.marker2.modelMatrix);
-        
-        colorVal = vec3.create();
-        colorVal[0] = 30;
-        colorVal[1] = 30;
-        colorVal[2] = 30;
 
-        program.colorVal = gl.getUniformLocation(program, 'colorVal');
-        gl.uniform3fv(program.colorVal, colorVal);
+		gl.uniform1i(gl.getUniformLocation(scene.program, "i"),4);
 		
-        gl.bindBuffer(gl.ARRAY_BUFFER, scene.marker2.vertexBuffer);
         gl.drawArrays(gl.TRIANGLES, 0, scene.marker2.vertexCount);
-        gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-        gl.readPixels(x, y, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, color);
-		console.log(color[0]);
-        if(task > 3){
-            if(color[0] == 30){
-                //   
-             }
-        }
-		//---------------------------
+        //---------------------------
 		
 		//Render marker 3
         gl.uniformMatrix4fv(scene.program.modelMatrixUniform, gl.FALSE,scene.marker3.modelMatrix);
         
-        colorVal = vec3.create();
-        colorVal[0] = 40;
-        colorVal[1] = 40;
-        colorVal[2] = 40;
 
-        program.colorVal = gl.getUniformLocation(program, 'colorVal');
-        gl.uniform3fv(program.colorVal, colorVal);
+		gl.uniform1i(gl.getUniformLocation(scene.program, "i"),5);
 		
-        gl.bindBuffer(gl.ARRAY_BUFFER, scene.marker3.vertexBuffer);
         gl.drawArrays(gl.TRIANGLES, 0, scene.marker3.vertexCount);
-        gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-        gl.readPixels(x, y, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, color);
-		console.log(color[0]);
-        if(task > 3){
-            if(color[0] == 40){
-                //   
-             }
-        }
-		//---------------------------
+        //---------------------------
 		
 		//Render marker 4
-        gl.uniformMatrix4fv(scene.program.modelMatrixUniform, gl.FALSE,scene.marker2.modelMatrix);
-        
-        colorVal = vec3.create();
-        colorVal[0] = 40;
-        colorVal[1] = 40;
-        colorVal[2] = 40;
+        gl.uniformMatrix4fv(scene.program.modelMatrixUniform, gl.FALSE,scene.marker4.modelMatrix);
 
-        program.colorVal = gl.getUniformLocation(program, 'colorVal');
-        gl.uniform3fv(program.colorVal, colorVal);
+		gl.uniform1i(gl.getUniformLocation(scene.program, "i"),6);
 		
-        gl.bindBuffer(gl.ARRAY_BUFFER, scene.marker4.vertexBuffer);
         gl.drawArrays(gl.TRIANGLES, 0, scene.marker4.vertexCount);
-        gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
         gl.readPixels(x, y, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, color);
-		console.log(color[0]);
-        if(task > 3){
-            if(color[0] == 40){
-                //   
-             }
-        }
+		
+		
+		gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+		gl.clear( gl.COLOR_BUFFER_BIT );
+		console.log("X " + x + " Y " + y + " COLOR: " + color[0] + "," + color[1] + "," + color[2] + "," + color[3]);
+		if(task > 2){
+			if(color[0] == 25){
+				if(event.which == 1){
+					mult += 0.05;
+				}
+				if(event.which == 3){
+					mult -= 0.05;
+				}
+				selectedMarker = -1;
+			}
+			else if(color[0] == 51){
+				if(event.which == 1){
+					mult2 += 0.05;
+				}
+				if(event.which == 3){
+					mult2 -= 0.05;
+				}
+				selectedMarker = -1;
+			}
+			else if(color[0] == 76){
+				selectedMarker = 0;
+				console.log("marker1");
+			}
+			else if(color[0] == 102){
+				selectedMarker = 0;
+				console.log("marker2");
+			}
+			else if(color[0] == 127){
+				selectedMarker = 1;
+				console.log("marker3");
+			}
+			else if(color[0] == 153){
+				selectedMarker = 1;
+				console.log("marker4");
+			}
+		}
 		//---------------------------
-        
+		
     });
+	
+	surface.addEventListener("mouseup", function(event){
+		selectedMarker = -1;
+		console.log("mouseup!");
+	});
+	
+	surface.addEventListener("mousemove", function(event){
+		if(selectedMarker == 0){
+			ypos1 = (((surface.height - event.clientY) - 270) * 0.006);
+			console.log(ypos1 + " " + (surface.height - event.clientY));
+		}
+		if(selectedMarker == 1){
+			ypos2 = (((surface.height - event.clientY) - 270) * 0.006);
+			console.log(ypos2 + " " + (surface.height - event.clientY));
+		}
+	});
 
 
     document.getElementById("task1").onclick = function(){
         task = 1;
-        mult = 1;
-        mult2 = 1;
+        mult = 0.1;
+        mult2 = 0.1;
+		ypos1 = 0.5;
+		ypos2 = -0.5;
         document.getElementById("text").innerHTML = "Task 1: Object translating side to side and rotating";
     }
     document.getElementById("task2").onclick = function(){
         task = 2;
-        mult = 1;
-        mult2 = 1;
+        mult = 0.1;
+        mult2 = 0.1;
+		ypos1 = 0.5;
+		ypos2 = -0.5;
 		document.getElementById("text").innerHTML = "Task 2: Second object, translating and rotating at a different rate";
     }
     document.getElementById("task3").onclick = function(){
         task = 3;
-		mult = 1;
-        mult2 = 1;
-		document.getElementById("text").innerHTML = "Task 3: Changing colour based on location<br>Left click on a object to speed it up<br>Right click on an object to slow it down";
+		mult = 0.1;
+        mult2 = 0.1;
+		ypos1 = 0.5;
+		ypos2 = -0.5;
+		document.getElementById("text").innerHTML = "Task 3: Changing colour based on location<br>Left click on a object to speed it up<br>Right click on an object to slow it down<br><br>NOTE WITH PICKING: Picking seems to be offset to the top left a small amount.<br>This seems to be due to the top left of the canvas not being located at the top left of the browser window, offsetting co-ordinates.";
     }
 	document.getElementById("task4").onclick = function(){
         task = 4;
-		mult = 1;
-        mult2 = 1;
-		document.getElementById("text").innerHTML = "Task 4: Markers...";
+		mult = 0.1;
+        mult2 = 0.1;
+		ypos1 = 0.5;
+		ypos2 = -0.5;
+		document.getElementById("text").innerHTML = "Task 4: Click and drag the markers vertically to change their position!<br><br>NOTE WITH PICKING: Picking seems to be offset to the top left a small amount.<br>This seems to be due to the top left of the canvas not being located at the top left of the browser window, offsetting co-ordinates.";
     }
       
     requestAnimationFrame(function(timestamp) {
@@ -708,12 +673,12 @@ $(document).ready(function() {
 function checkxPosition(movingDirec, modelMatrix){
     
     if(movingDirec == 0.05){
-        if(modelMatrix[12] >= 2){
+        if(modelMatrix[12] >= 1.2){
             movingDirec = -0.05;
             
         }
     }else{
-        if(modelMatrix[12] <= -2){
+        if(modelMatrix[12] <= -1.2){
             movingDirec = 0.05;
         }
     }
