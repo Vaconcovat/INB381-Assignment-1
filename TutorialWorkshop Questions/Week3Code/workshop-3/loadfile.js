7f4bb64cc6fd46beb8dc51c8ca39b402c264135c
Vaconcovat <vaconcovat@gmail.com>
2016-10-21 23:24:08 +1000

    Removed assignment 1 stuff
    added assignment 2 specifications

--- TutorialWorkshop Questions/Week3Code/workshop-3/loadfile.js
+++
@@ -1,6 +0,0 @@
-window.onload = function init() {
-var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", 'C:/Fred/workshop-3/cone.obj', false ); // false for synchronous request
-    xmlHttp.send( null );
-   console.log(xmlHttp.responseText);
-}
